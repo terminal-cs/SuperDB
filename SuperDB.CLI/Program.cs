a0f0b1078196991ca42c7a71a536f39c644314a2
Evan Lawrence <evanlawrence@cyberfear.com>
2022-10-12 13:48:27 -0700

    Impliment all commands to CLI.

--- SuperDB.CLI/Program.cs
+++ SuperDB.CLI/Program.cs
@@ -62,6 +62,62 @@ while (true)
 						}
 						Console.WriteLine(DB.ReadLong(Split[2]));
 						break;
+					case "uint":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						Console.WriteLine(DB.ReadUInt(Split[2]));
+						break;
+					case "int":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						Console.WriteLine(DB.ReadInt(Split[2]));
+						break;
+					case "ushort":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						Console.WriteLine(DB.ReadUShort(Split[2]));
+						break;
+					case "short":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						Console.WriteLine(DB.ReadUShort(Split[2]));
+						break;
+					case "double":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						Console.WriteLine(DB.ReadDouble(Split[2]));
+						break;
+					case "float":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						Console.WriteLine(DB.ReadFloat(Split[2]));
+						break;
+					case "byte":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						Console.WriteLine(DB.ReadByte(Split[2]));
+						break;
 					default:
 						Console.WriteLine($"Unknown or unsupported type '{Split[1]}'");
 						break;
@@ -79,6 +135,78 @@ while (true)
 							continue;
 						}
 						DB.WriteString(Split[2], Split[3]);
+						break;
+					case "ulong":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						DB.WriteULong(Split[2], ulong.Parse(Split[3]));
+						break;
+					case "long":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						DB.WriteLong(Split[2], long.Parse(Split[3]));
+						break;
+					case "uint":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						DB.WriteUInt(Split[2], uint.Parse(Split[3]));
+						break;
+					case "int":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						DB.WriteInt(Split[2], int.Parse(Split[3]));
+						break;
+					case "ushort":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						DB.WriteUShort(Split[2], ushort.Parse(Split[3]));
+						break;
+					case "short":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						DB.WriteShort(Split[2], short.Parse(Split[3]));
+						break;
+					case "double":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						DB.WriteDouble(Split[2], double.Parse(Split[3]));
+						break;
+					case "float":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						DB.WriteFloat(Split[2], float.Parse(Split[3]));
+						break;
+					case "byte":
+						if (DB == null)
+						{
+							Console.WriteLine("Database is not loaded.");
+							continue;
+						}
+						DB.WriteByte(Split[2], byte.Parse(Split[3]));
 						break;
 					default:
 						Console.WriteLine($"Unknown or unsupported type '{Split[1]}'");
@@ -114,6 +242,11 @@ while (true)
 				{
 					Console.WriteLine(T[I]);
 				}
+				break;
+			#endregion
+			#region Exit
+			case "exit":
+				Environment.Exit(0);
 				break;
 			#endregion
 			#region Default
